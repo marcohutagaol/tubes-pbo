195e21fda629927517a0da78dc771687e9d88a02
marcohutagaol <marco3003207@gmail.com>
2025-05-21 17:17:50 +0700

    saldo part 2

--- saldo.cpp
+++ saldo.cpp
@@ -3,26 +3,80 @@ using namespace std;
 
 class Saldo
 {
-protected:
-  int saldo = 1;
-
 public:
-  
-};
+  int saldo;
 
-class fiturSaldo : public Saldo {
-  public:
-  void cekSaldo() {
-    cout << "Jumlah saldo anda: " << saldo;
+  Saldo(int saldo = 0) {
+    this->saldo = saldo;
   }
 
-  void setorSaldo() {
-    cout << "Masukkan jumlah saldo yang ingin disetor: " << saldo;
+  Saldo operator+(Saldo const &temp)
+  {
+    Saldo sum;
+    sum.saldo = saldo + temp.saldo;
+    return sum;
+  }
+
+  Saldo operator-(Saldo const &temp) 
+  {
+    Saldo diff;
+    diff.saldo = saldo - temp.saldo;
+    return diff;
+  }
+
+  void cekSaldo()
+  {
+    cout << "Jumlah saldo anda: " << saldo << endl;
+  }
+};
+
+class fiturSaldo : public Saldo
+{
+public:
+  void setSaldo(int saldo)
+  {
+    this->saldo = saldo;
+  }
+
+  int getSaldo()
+  {
+    return saldo;
+  }
+
+  void setorSaldo()
+  {
+    cout << "Masukkan jumlah saldo yang ingin disetor: ";
+    cin >> saldo;
+  }
+
+  void tarikSaldo()
+  {
+    cout << "Masukkan jumlah saldo yang ingin ditarik: ";
+    cin >> saldo;
   }
 };
 
 int main()
 {
-  fiturSaldo user;
-  user.cekSaldo();
+  fiturSaldo saldo_awal;
+  saldo_awal.setSaldo(1000);
+  saldo_awal.getSaldo();
+  saldo_awal.cekSaldo();
+
+  // fiturSaldo saldo_setor;
+  // saldo_setor.setorSaldo();
+  // saldo_setor.getSaldo();
+
+  // Saldo tambah_saldo;
+  // tambah_saldo = saldo_awal + saldo_setor;
+  // tambah_saldo.cekSaldo();
+
+  fiturSaldo saldo_tarik;
+  saldo_tarik.tarikSaldo();
+
+  Saldo kurang_saldo;
+  kurang_saldo = saldo_awal - saldo_tarik;
+  kurang_saldo.cekSaldo();
+
+  return 0;
 }
